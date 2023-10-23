622b55cdd353ceb9d8feaaac0a5f4a63b36cc0fc
caiquelms <caique.lemosferreira7@gmail.com>
2023-10-23 18:13:07 -0300

    correção imagem e arquivo imsomnia att

--- grupo4/src/main/java/com/trabalhofinal/grupo4/controllers/ProdutoController.java
+++ grupo4/src/main/java/com/trabalhofinal/grupo4/controllers/ProdutoController.java
@@ -1,9 +1,11 @@
 package com.trabalhofinal.grupo4.controllers;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,51 +21,60 @@ import org.springframework.web.multipart.MultipartFile;
 import com.trabalhofinal.grupo4.entities.Produto;
 import com.trabalhofinal.grupo4.services.ProdutoServices;
 
-	@RestController
-	@RequestMapping("/produtos")
-	public class ProdutoController {
-		
-		@Autowired
-		ProdutoServices produtoServices;
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
 
-		@GetMapping
-		public ResponseEntity<List<Produto>> listarProdutos() {
-			return new ResponseEntity<>(produtoServices.listarProdutoServices(), HttpStatus.OK);
-		}
+	@Autowired
+	ProdutoServices produtoServices;
 
-		@GetMapping("/{id}")
-		public ResponseEntity<Produto> buscarPorId(@PathVariable Integer id) {
-			Produto produto = produtoServices.buscarProdutoServicesPorId(id);
-			if (produto == null)
-				return new ResponseEntity<>(produto, HttpStatus.NOT_FOUND);
-			else
-				return new ResponseEntity<>(produto, HttpStatus.OK);
-		}
+	@GetMapping
+	public ResponseEntity<List<Produto>> listarProdutos() {
+		return new ResponseEntity<>(produtoServices.listarProdutoServices(), HttpStatus.OK);
+	}
 
-		@PostMapping
-		public ResponseEntity<Produto> salvar(@RequestBody Produto produto) {
-			return new ResponseEntity<>(produtoServices.salvarProdutoServices(produto), HttpStatus.CREATED);
-		}
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> buscarPorId(@PathVariable Integer id) {
+		Produto produto = produtoServices.buscarProdutoServicesPorId(id);
+		if (produto == null)
+			return new ResponseEntity<>(produto, HttpStatus.NOT_FOUND);
+		else
+			return new ResponseEntity<>(produto, HttpStatus.OK);
+	}
 
-		@PutMapping
-		public ResponseEntity<Produto> atualizar(@RequestBody Produto produto) {
-			return new ResponseEntity<>(produtoServices.atualizarProdutoServices(produto), HttpStatus.OK);
-		}
+//		@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE })
+//		public ResponseEntity<Produto> salvar(@RequestBody Produto produto) {
+//			return new ResponseEntity<>(produtoServices.salvarProdutoServices(produto), HttpStatus.CREATED);
+//		}
 
-		@DeleteMapping
-		public ResponseEntity<String> deletarProduto(@RequestBody Produto produto) {
-			if (Boolean.TRUE.equals(produtoServices.deletarProduto(produto))) {
-				return new ResponseEntity<>("Deletado com sucesso", HttpStatus.OK);
-			} else {
-				return new ResponseEntity<>("Não foi possivel deletar", HttpStatus.BAD_REQUEST);
-			}
-		}
-			
-		public ResponseEntity<Produto> cadastrar(@RequestPart("produto") Produto produto, @RequestPart("foto") MultipartFile foto) {
+	@PutMapping
+	public ResponseEntity<Produto> atualizar(@RequestBody Produto produto) {
+		return new ResponseEntity<>(produtoServices.atualizarProdutoServices(produto), HttpStatus.OK);
+	}
 
-			Produto produtoDTO = produtoServices.salvarProdutoServices(produto);
-			produtoServices.salvarFoto(foto);
-			return new ResponseEntity<>(produtoDTO, HttpStatus.CREATED); //201
-			
+	@DeleteMapping
+	public ResponseEntity<String> deletarProduto(@RequestBody Produto produto) {
+		if (Boolean.TRUE.equals(produtoServices.deletarProduto(produto))) {
+			return new ResponseEntity<>("Deletado com sucesso", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("Não foi possivel deletar", HttpStatus.BAD_REQUEST);
 		}
+	}
+
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE })
+	public ResponseEntity<Produto> cadastrar(@RequestPart("produto") Produto produto,
+			@RequestPart("foto") MultipartFile foto) {
+
+		Produto produtoDTO = produtoServices.salvarProdutoServices(produto);
+		produtoServices.salvarFoto(foto);
+		return new ResponseEntity<>(produtoDTO, HttpStatus.CREATED); // 201
+
+	}
+	
+	@PostMapping("/comfoto")
+	public ResponseEntity<Produto> salvarComFoto(@RequestPart("produto") String Produto,
+			@RequestPart("foto") MultipartFile foto
+			) throws IOException{
+		return new ResponseEntity<>(produtoServices.salvarProdutoComFoto(Produto, foto), HttpStatus.CREATED);
+	}
 }
--- grupo4/src/main/java/com/trabalhofinal/grupo4/services/ProdutoServices.java
+++ grupo4/src/main/java/com/trabalhofinal/grupo4/services/ProdutoServices.java
@@ -9,30 +9,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trabalhofinal.grupo4.entities.Produto;
 import com.trabalhofinal.grupo4.repository.ProdutoRepository;
 
+import io.jsonwebtoken.io.IOException;
+
 @Service
 public class ProdutoServices {
-	
-	@Autowired  
+
+	@Autowired
 	ProdutoRepository produtoRepo;
-	
-	//IMAGEM
+
+	// IMAGEM
 	private String raiz = "C:\\DEVSERRATEC\\TrabalhoFinalApirestful\\grupo4\\src\\main\\java\\com\\trabalhofinal\\grupo4";
-		
-	//diretorio das fotos
+
+	// diretorio das fotos
 	private String diretorioFotos = "fotos";
 
 	public void salvarFoto(MultipartFile foto) {
 		this.salvar(this.diretorioFotos, foto);
 	}
-		
+
 	public void salvar(String diretorio, MultipartFile arquivo) {
 		// diretorio
 		Path diretorioPath = Path.of(this.raiz, diretorio); // Use Path.of para criar o caminho.
 
-		// diretorio + arquivo 
+		// diretorio + arquivo
 		Path arquivoPath = diretorioPath.resolve(arquivo.getOriginalFilename());
 
 		try {
@@ -46,32 +50,50 @@ public class ProdutoServices {
 	public List<Produto> listarProdutoServices() {
 		return produtoRepo.findAll();
 	}
-	
+
 	public Produto buscarProdutoServicesPorId(Integer id) {
 		return produtoRepo.findById(id).orElse(null);
 	}
-	
+
 	public Produto salvarProdutoServices(Produto produtoServices) {
 		return produtoRepo.save(produtoServices);
 	}
-	
+
 	public Produto atualizarProdutoServices(Produto produtoServices) {
 		return produtoRepo.save(produtoServices);
 	}
-	
+
 	public Boolean deletarProduto(Produto produto) {
-        if (produto == null)
-            return false;
+		if (produto == null)
+			return false;
 
-        Produto produtoExistente = buscarProdutoServicesPorId(produto.getIdProduto());
+		Produto produtoExistente = buscarProdutoServicesPorId(produto.getIdProduto());
 
-        if (produtoExistente == null)
-            return false;
+		if (produtoExistente == null)
+			return false;
 
-        produtoRepo.delete(produto);
+		produtoRepo.delete(produto);
 
-        Produto produtoContinuaExistindo = buscarProdutoServicesPorId(produto.getIdProduto());
+		Produto produtoContinuaExistindo = buscarProdutoServicesPorId(produto.getIdProduto());
 
-        return produtoContinuaExistindo == null;
-    }
+		return produtoContinuaExistindo == null;
+	}
+
+	public Produto salvarProdutoComFoto(String strProduto, MultipartFile arqImg)
+			throws java.io.IOException {
+		Produto produto = new Produto();
+
+		try {
+			ObjectMapper objMp = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+			produto = objMp.readValue(strProduto, Produto.class);
+		} catch (IOException e) {
+			System.out.println("Erro ao converter a string Produto: " + e.toString());
+		}
+
+//editora.setImagemFileName(arqImg.getBytes());
+		produto.setImagem(arqImg.getBytes());
+
+		return produtoRepo.save(produto);
+	}
 }
